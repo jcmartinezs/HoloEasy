fe260c5e4cf40a2907e3d2104150b2e7d2ea58f7
hariza17 <hernando.ariza@ormuco.com>
2018-04-21 16:02:45 -0500

    changing config file, setting db conection

--- modulos/config.py
+++ modulos/config.py
@@ -4,10 +4,10 @@ APP_CONFIG = {
     'bd_host': 'localhost',
     'bd_nombre': 'bd_holoeasy',
     'bd_usuario': 'root',
-    'bd_password': 'Lenier111092.',
+    'bd_password': 'root.',
     'bd_port': 3306,
-    'ruta':'/home/lenier/Downloads/holoeasy/',#/var/www/holoeasy/
-    'S':'/',
+    'ruta': '/var/www/HoloEasy/',
+    'S': '/',
     'email_user': 'lenierdiaz11@gmail.com',
     'email_password': 'lenier111092',
     'email_to': 'leledipac@gmail.com',
